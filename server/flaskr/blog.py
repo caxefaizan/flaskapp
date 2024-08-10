ca31580121c1e7d55b7f99d9554200a2f1d874bd
faizan.qazi <faizanzahid09@hotmail.com>
2024-08-10 15:47:59 +0530

    refactor

--- server/flaskr/blog.py
+++ server/flaskr/blog.py
@@ -10,8 +10,9 @@ from flaskr.common import (
     get_db,
     get_messages,
     get_profiles,
-    get_values,
-    getProfileData,
+    createUpdateProfileData,
+    preRouteChecks,
+    deleteProfileData,
 )
 
 bp = Blueprint("blog", __name__)
@@ -28,12 +29,23 @@ def index():
 @bp.route("/<username>/profile/view", methods=("GET",))
 @login_required
 def viewProfile(username):
-    count = get_visitor_count()
-    profileData = getProfileData(username)
+    action = request.path.split("/")[-1]
+    # if not action in ["view", "create", "edit", "delete"]:
+    #     abort(404, "Page not found!")
+    count, profileData = preRouteChecks(g, action, username)
+
+    # if action in ["create", "edit"] and request.method == "POST":
+    #     createUpdateProfileData(username, request.form, action)
+    #     flash("Details saved successfully")
+    #     return redirect(url_for("blog.viewProfile", username=username, action='view'))
+
+    # if action == "create" and profileData[0]:
+    #     return redirect(url_for("blog.viewProfile", username=username, action='view'))
+
     return render_template(
         "blog/profile.html",
         username=username,
-        profileData=profileData,
+        profileData=None if action == "create" else profileData,
         sections=sections,
         profileInputs=profileInputs,
         visitor_count=count,
@@ -43,79 +55,13 @@ def viewProfile(username):
 @bp.route("/<username>/profile/edit", methods=("GET", "POST"))
 @login_required
 def editProfile(username):
-    if g.user["username"] != username:
-        abort(403, f"Unauthorized!")
-
-    count = get_visitor_count()
-    profileData = getProfileData(username)
+    action = request.path.split("/")[-1]
+    count, profileData = preRouteChecks(g, action, username)
 
     if request.method == "POST":
-        db = get_db()
-        db.execute(
-            "UPDATE client SET clientGender = ?, clientCast = ?, clientOccupation = ?, clientEducation = ?, clientAge = ?, clientHeight = ?, clientComplexion = ?"
-            f"WHERE token = '{username}'",
-            (
-                request.form["clientGender"],
-                request.form["clientCast"],
-                request.form["clientOccupation"],
-                request.form["clientEducation"],
-                int(request.form["clientAge"]),
-                float(request.form["clientHeight"]),
-                request.form["clientComplexion"],
-            ),
-        )
-        db.execute(
-            "UPDATE preference SET preferenceGender = ?, preferenceOccupation = ?, preferenceEducation = ?, preferenceAge = ?, preferenceHeight = ?, preferencecomplexion = ?"
-            f"WHERE token = '{username}'",
-            (
-                "Female" if request.form["clientGender"] == "Male" else "Male",
-                request.form["preferenceOccupation"],
-                request.form["preferenceEducation"],
-                int(request.form["preferenceAge"]),
-                float(request.form["preferenceHeight"]),
-                request.form["preferenceComplexion"],
-            ),
-        )
-        db.execute(
-            "UPDATE parents SET fathersOccupation = ?, mothersOccupation = ?, mothersCast = ?, otherRelations = ?"
-            f"WHERE token = '{username}'",
-            (
-                request.form["fathersOccupation"],
-                request.form["mothersOccupation"],
-                request.form["mothersCast"],
-                request.form["otherRelations"],
-            ),
-        )
-        db.execute(
-            "UPDATE lifestyle SET smoking = ?, prayers = ?, religiousSect = ?"
-            f"WHERE token = '{username}'",
-            (
-                request.form["smoking"],
-                request.form["prayers"],
-                request.form["religiousSect"],
-            ),
-        )
-        db.execute(
-            "UPDATE residence SET presentAddress = ?, oldAddress = ?"
-            f"WHERE token = '{username}'",
-            (
-                request.form["presentAddress"],
-                request.form["oldAddress"],
-            ),
-        )
-        db.execute(
-            "UPDATE sibling SET siblingGender = ?, siblingOccupation = ?, siblingSpouseCast = ?, siblingSpouseOccupation = ?"
-            f"WHERE token = '{username}'",
-            (
-                request.form["siblingGender"],
-                request.form["siblingOccupation"],
-                request.form["siblingSpouseCast"],
-                request.form["siblingSpouseOccupation"],
-            ),
-        )
-        db.commit()
+        createUpdateProfileData(username, request.form, action)
         flash("Details saved successfully")
-        return redirect(url_for("blog.viewProfile", username=username))
+        return redirect(url_for("blog.viewProfile", username=username, action=action))
 
     return render_template(
         "blog/profile.html",
@@ -130,92 +76,21 @@ def editProfile(username):
 @bp.route("/<username>/profile/create", methods=("GET", "POST"))
 @login_required
 def createProfile(username):
-    if g.user["username"] != username:
-        abort(403, f"Unauthorized!")
+    action = request.path.split("/")[-1]
+    count, profileData = preRouteChecks(g, action, username)
 
-    count = get_visitor_count()
-    profile = get_values(username, "client")
-
-    if profile:
-        return redirect(url_for("blog.viewProfile", username=username))
     if request.method == "POST":
-        db = get_db()
-        db.execute(
-            "INSERT INTO client (clientGender, token, clientCast, clientOccupation, clientEducation, clientAge, clientHeight, clientComplexion)"
-            " VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-            (
-                request.form["clientGender"],
-                username,
-                request.form["clientCast"],
-                request.form["clientOccupation"],
-                request.form["clientEducation"],
-                int(request.form["clientAge"]),
-                float(request.form["clientHeight"]),
-                request.form["clientComplexion"],
-            ),
-        )
-        db.execute(
-            "INSERT INTO preference (preferenceGender, token, preferenceOccupation, preferenceEducation, preferenceAge, preferenceHeight, preferencecomplexion)"
-            " VALUES (?, ?, ?, ?, ?, ?, ?)",
-            (
-                "Female" if request.form["clientGender"] == "Male" else "Male",
-                username,
-                request.form["preferenceOccupation"],
-                request.form["preferenceEducation"],
-                int(request.form["preferenceAge"]),
-                float(request.form["preferenceHeight"]),
-                request.form["preferenceComplexion"],
-            ),
-        )
-        db.execute(
-            "INSERT INTO parents (token, fathersOccupation, mothersOccupation, mothersCast, otherRelations)"
-            " VALUES (?, ?, ?, ?, ?)",
-            (
-                username,
-                request.form["fathersOccupation"],
-                request.form["mothersOccupation"],
-                request.form["mothersCast"],
-                request.form["otherRelations"],
-            ),
-        )
-        db.execute(
-            "INSERT INTO lifestyle (token, smoking, prayers, religiousSect)"
-            " VALUES (?, ?, ?, ?)",
-            (
-                username,
-                request.form["smoking"],
-                request.form["prayers"],
-                request.form["religiousSect"],
-            ),
-        )
-        db.execute(
-            "INSERT INTO residence (token, presentAddress, permanentAddress, oldAddress)"
-            " VALUES (?, ?, ?, ?)",
-            (
-                username,
-                request.form["presentAddress"],
-                request.form["permanentAddress"],
-                request.form["oldAddress"],
-            ),
-        )
-        db.execute(
-            "INSERT INTO sibling (token, siblingGender, siblingOccupation, siblingSpouseCast, siblingSpouseOccupation)"
-            " VALUES (?, ?, ?, ?, ?)",
-            (
-                username,
-                request.form["siblingGender"],
-                request.form["siblingOccupation"],
-                request.form["siblingSpouseCast"],
-                request.form["siblingSpouseOccupation"],
-            ),
-        )
-        db.commit()
+        createUpdateProfileData(username, request.form, action)
         flash("Details saved successfully")
-        return redirect(url_for("blog.viewProfile", username=username))
+        return redirect(url_for("blog.viewProfile", username=username, action=action))
+
+    if profileData[0]:
+        return redirect(url_for("blog.viewProfile", username=username, action=action))
 
     return render_template(
         "blog/profile.html",
         username=username,
+        profileData=None,
         sections=sections,
         profileInputs=profileInputs,
         visitor_count=count,
@@ -225,30 +100,12 @@ def createProfile(username):
 @bp.route("/<username>/profile/delete", methods=("POST",))
 @login_required
 def deleteProfile(username):
-    if g.user["username"] != username:
-        abort(403, f"Unauthorized!")
-    profile = get_values(username, "client")
-    if not profile is None:
+    action = request.path.split("/")[-1]
+    _, profileData = preRouteChecks(g, action, username)
+
+    if not profileData[0] is None:
         db = get_db()
-        db.execute(
-            f"DELETE FROM client WHERE token = '{username}'",
-        )
-        db.execute(
-            f"DELETE FROM lifestyle WHERE token = '{username}'",
-        )
-        db.execute(
-            f"DELETE FROM parents WHERE token = '{username}'",
-        )
-        db.execute(
-            f"DELETE FROM preference WHERE token = '{username}'",
-        )
-        db.execute(
-            f"DELETE FROM residence WHERE token = '{username}'",
-        )
-        db.execute(
-            f"DELETE FROM sibling WHERE token = '{username}'",
-        )
-        db.commit()
+        deleteProfileData(username, db)
         flash("Deleted successfully")
     return redirect(url_for("blog.index"))
 
@@ -306,6 +163,5 @@ def directMessage(username):
 @bp.route("/<username>/account", methods=("GET", "POST"))
 @login_required
 def account(username):
-    # post = get_post(username)
     count = get_visitor_count()
     return render_template("blog/account.html", username=username, visitor_count=count)
--- server/flaskr/common.py
+++ server/flaskr/common.py
@@ -1,7 +1,9 @@
 from flaskr.db import get_db
 import sqlite3
 import itertools
-
+from flaskr.visitors import get_visitor_count
+from werkzeug.exceptions import abort
+from flask import request
 
 def get_messages(userId, receiver, size, check_author=True) -> sqlite3.Row:
     messageSize = int(size) * 5
@@ -115,3 +117,176 @@ def getProfileData(username):
     residence = get_values(username, "residence")
 
     return [profile, sibling, parents, preference, lifestyle, residence]
+
+
+def createUpdateProfileData(username, formData, action):
+    db = get_db()
+    if action == "create":
+        db.execute(
+            "INSERT INTO client (clientGender, token, clientCast, clientOccupation, clientEducation, clientAge, clientHeight, clientComplexion)"
+            " VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+            (
+                formData["clientGender"],
+                username,
+                formData["clientCast"],
+                formData["clientOccupation"],
+                formData["clientEducation"],
+                int(formData["clientAge"]),
+                float(formData["clientHeight"]),
+                formData["clientComplexion"],
+            ),
+        )
+        db.execute(
+            "INSERT INTO preference (preferenceGender, token, preferenceOccupation, preferenceEducation, preferenceAge, preferenceHeight, preferencecomplexion)"
+            " VALUES (?, ?, ?, ?, ?, ?, ?)",
+            (
+                "Female" if formData["clientGender"] == "Male" else "Male",
+                username,
+                formData["preferenceOccupation"],
+                formData["preferenceEducation"],
+                int(formData["preferenceAge"]),
+                float(formData["preferenceHeight"]),
+                formData["preferenceComplexion"],
+            ),
+        )
+        db.execute(
+            "INSERT INTO parents (token, fathersOccupation, mothersOccupation, mothersCast, otherRelations)"
+            " VALUES (?, ?, ?, ?, ?)",
+            (
+                username,
+                formData["fathersOccupation"],
+                formData["mothersOccupation"],
+                formData["mothersCast"],
+                formData["otherRelations"],
+            ),
+        )
+        db.execute(
+            "INSERT INTO lifestyle (token, smoking, prayers, religiousSect)"
+            " VALUES (?, ?, ?, ?)",
+            (
+                username,
+                formData["smoking"],
+                formData["prayers"],
+                formData["religiousSect"],
+            ),
+        )
+        db.execute(
+            "INSERT INTO residence (token, presentAddress, permanentAddress, oldAddress)"
+            " VALUES (?, ?, ?, ?)",
+            (
+                username,
+                formData["presentAddress"],
+                formData["permanentAddress"],
+                formData["oldAddress"],
+            ),
+        )
+        db.execute(
+            "INSERT INTO sibling (token, siblingGender, siblingOccupation, siblingSpouseCast, siblingSpouseOccupation)"
+            " VALUES (?, ?, ?, ?, ?)",
+            (
+                username,
+                formData["siblingGender"],
+                formData["siblingOccupation"],
+                formData["siblingSpouseCast"],
+                formData["siblingSpouseOccupation"],
+            ),
+        )
+    else:
+        db.execute(
+            "UPDATE client SET clientGender = ?, clientCast = ?, clientOccupation = ?, clientEducation = ?, clientAge = ?, clientHeight = ?, clientComplexion = ?"
+            f"WHERE token = '{username}'",
+            (
+                formData["clientGender"],
+                formData["clientCast"],
+                formData["clientOccupation"],
+                formData["clientEducation"],
+                int(formData["clientAge"]),
+                float(formData["clientHeight"]),
+                formData["clientComplexion"],
+            ),
+        )
+        db.execute(
+            "UPDATE preference SET preferenceGender = ?, preferenceOccupation = ?, preferenceEducation = ?, preferenceAge = ?, preferenceHeight = ?, preferencecomplexion = ?"
+            f"WHERE token = '{username}'",
+            (
+                "Female" if formData["clientGender"] == "Male" else "Male",
+                formData["preferenceOccupation"],
+                formData["preferenceEducation"],
+                int(formData["preferenceAge"]),
+                float(formData["preferenceHeight"]),
+                formData["preferenceComplexion"],
+            ),
+        )
+        db.execute(
+            "UPDATE parents SET fathersOccupation = ?, mothersOccupation = ?, mothersCast = ?, otherRelations = ?"
+            f"WHERE token = '{username}'",
+            (
+                formData["fathersOccupation"],
+                formData["mothersOccupation"],
+                formData["mothersCast"],
+                formData["otherRelations"],
+            ),
+        )
+        db.execute(
+            "UPDATE lifestyle SET smoking = ?, prayers = ?, religiousSect = ?"
+            f"WHERE token = '{username}'",
+            (
+                formData["smoking"],
+                formData["prayers"],
+                formData["religiousSect"],
+            ),
+        )
+        db.execute(
+            "UPDATE residence SET presentAddress = ?, oldAddress = ?"
+            f"WHERE token = '{username}'",
+            (
+                formData["presentAddress"],
+                formData["oldAddress"],
+            ),
+        )
+        db.execute(
+            "UPDATE sibling SET siblingGender = ?, siblingOccupation = ?, siblingSpouseCast = ?, siblingSpouseOccupation = ?"
+            f"WHERE token = '{username}'",
+            (
+                formData["siblingGender"],
+                formData["siblingOccupation"],
+                formData["siblingSpouseCast"],
+                formData["siblingSpouseOccupation"],
+            ),
+        )
+    db.commit()
+
+
+def preRouteChecks(g, action, username):
+    if action != "view":
+        if g.user["username"] != username:
+            abort(403, f"Unauthorized!")
+
+    if (action == "delete" and request.method == "GET") or (action == "view" and request.method == "POST"):
+        abort(405, f"Method not allowed!")
+
+    count = get_visitor_count()
+    profileData = getProfileData(username)
+
+    return count, profileData
+
+def deleteProfileData(username, db):
+    db.execute(
+            f"DELETE FROM client WHERE token = '{username}'",
+        )
+    db.execute(
+        f"DELETE FROM lifestyle WHERE token = '{username}'",
+    )
+    db.execute(
+        f"DELETE FROM parents WHERE token = '{username}'",
+    )
+    db.execute(
+        f"DELETE FROM preference WHERE token = '{username}'",
+    )
+    db.execute(
+        f"DELETE FROM residence WHERE token = '{username}'",
+    )
+    db.execute(
+        f"DELETE FROM sibling WHERE token = '{username}'",
+    )
+    db.commit()
